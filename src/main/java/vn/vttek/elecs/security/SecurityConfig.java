3c98dbdc121d63e8f1b98bf6fb554deca2da1c9a
manhcuongk55 <dmcksclck55@gmail.com>
2019-08-25 13:54:31 +0700

    Fix code

--- src/main/java/vn/vttek/elecs/security/SecurityConfig.java
+++ src/main/java/vn/vttek/elecs/security/SecurityConfig.java
@@ -3,7 +3,9 @@ package vn.vttek.elecs.security;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -42,6 +44,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authoritiesByUsernameQuery("select username, role from user_roles where username=?");
                 
 
+    }
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
 
